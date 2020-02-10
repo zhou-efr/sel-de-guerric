d5209c2795f02a2642365dfa6d7b73073d60e8be
gundamzhou <53347566+gundamzhou@users.noreply.github.com>
2020-02-10 10:57:13 +0100

    Update areaLoader.py

--- sources/areaLoader.py
+++ sources/areaLoader.py
@@ -2,6 +2,9 @@ import pygame
 
 pygame.init()
 
+
+
+
 def strToArray(text):
     arrayProduce = []
     for i in text:
@@ -10,8 +13,8 @@ def strToArray(text):
     return arrayProduce
 #--end strToArray---
 
-def areaLoader(environment, level, board):
-    """area loader .py is the file which containt the loader functions.
+class areaLoader:
+    """area loader is the class containing all the necessary contained in folders
 
 
     image folder achitexture (c'est des fichier text xD)
@@ -41,35 +44,65 @@ def areaLoader(environment, level, board):
     the player always spawn at board's index 00 and the arival board is always index 99
 
     """
-    #setting internal variables
-    environment = environment
-    level = level
-    board = board
-    adress = "C:/Users/gundamzhou/Documents/GitHub/sel-de-guerric/files/environment" + str( environment) + "/level" + str( level) + "/board" + str( board) + ".txt"
-    contents = None
-    print( adress)
-    area = []
+    def __init__(self,environment, level, board):
+        #setting internal variables
+        self.environment = environment
+        self.level = level 
+        self.board = board
+        self.adress = "C:/Users/gundamzhou/Documents/GitHub/sel-de-guerric/files/environment" + str( environment) + "/level" + str( level)
+        self.boardAdress = self.adress + "/board" + self.board + ".txt"
+        self.backAdress = self.adress + "/back" + self.board + ".png"
+        self.area = []
 
-    #beginning of initialazation 
-    try:
-        with open( adress, 'r') as file:
-            contents = file.read().split('\n')
-        #---end with---
+        #beginning of initialazation 
+        self.initBoard()
+    #---end init---
 
-        for i in range(len(contents)):
-            contents[i] = strToArray(contents[i])
-        #---end for---
+    def initBoard(self):
+        #board initialazation
+        contents = None 
 
-        area = contents
-        
-    except FileNotFoundError:
-        print("file not found")
-    
-    return area
+        try:
+            with open(self.adress, 'r') as file:
+                contents = file.read().split('\n')
+            #---end with---
+
+            for i in range(len(contents)):
+                contents[i] = strToArray(contents[i])
+            #---end for---
+
+            self.area = contents
+            
+        except FileNotFoundError:
+            print("file not found")
+        #---end try---
+    #---end initBoard---
+
+    def getBackAdress(self):
+        return self.backAdress
+
+    def getBoardAdress(self):
+        return self.boardAdress
+
+    def getAdress(self):
+        return self.adress
+
+    def getLevel(self):
+        return self.level
+
+    def getArray(self):
+        return self.area
+
+    def getEnvironment(self):
+        return self.environment
 #---end AreaLoader---
 
 def areaPrinter(surface, board, environment):
-    print("wip")
+    """
+        surface : a pygame surface
+        board : 2d array containing keyChar
+        environment : environment object 
+    """
 #---end areaPrinter---
 
 print(areaLoader(1,1,1))
